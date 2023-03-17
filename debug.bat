b510eb7cd2722d5d8c4cca2db02a789d70ee7a90
MultiTheFranky <fran.minecraft.mta@gmail.com>
2023-03-17 23:32:33 +0100

    fix: debug

--- debug.bat
+++ debug.bat
@@ -1,7 +1,7 @@
 @echo off
 rem =========ZONE TO EDIT========
 rem Define Disk
-set ARMA3_PARAMS=-mod=@CBA_A3;@ace;z\lince -world=empty -noPause -showScriptErrors -filePatching
+set ARMA3_PARAMS=-mod=!Workshop\@CBA_A3;!Workshop\@ace;z\lince -world=empty -noPause -showScriptErrors -filePatching -skipIntro -noSplash -noPause -noPauseAudio
 rem Define Arma 3 directory
 set ARMA3_DIR="C:\Program Files (x86)\Steam\steamapps\common\Arma 3"
 rem =============================
@@ -12,8 +12,8 @@ set ARMA3_EXE=%ARMA3_DIR%\arma3_x64.exe
 rem Check if Arma 3 executable exists
 if not exist %ARMA3_EXE% (
   echo Arma 3 executable does not exist, please check the path.
-  goto exit
+  exit
 )
 rem Run Arma 3
 start "" %ARMA3_EXE% %ARMA3_PARAMS%
-pause
+exit
